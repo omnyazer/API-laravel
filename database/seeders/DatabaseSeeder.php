1bfeff869c4357dc21cced6f26067edae3c0d802
omnya zerghouni <omnyazer@gmail.com>
2026-02-09 16:55:47 +0100

    corrections

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\BookSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    
     public function run(): void
     {
         $this->call([
